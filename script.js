a2079b9fcbe9e4f99b29c2583aff3dd34bc98a59
DanyMencu <dany69men@gmail.com>
2021-11-04 14:47:32 +0100

    Team members array create

--- script.js
+++ script.js
@@ -3,3 +3,37 @@
 //*2- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
 //*3- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
 
+//* 1.
+const teamMembers = [
+    {
+        image: 'img/wayne-barnett-founder-ceo.jpg',
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+    },
+    {
+        image: 'img/angela-caroll-chief-editor.jpg',
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+    },
+    {
+        image: 'img/walter-gordon-office-manager.jpg',
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+    },
+    {
+        image: 'img/angela-lopez-social-media-manager.jpg',
+        name: 'Angela lopez',
+        role: 'Social Media Manager',
+    },
+    {
+        image: 'img/scott-estrada-developer.jpg',
+        name: 'Scott Estrada',
+        role: 'Developer',
+    },
+    {
+        image: 'img/barbara-ramos-graphic-designer.jpg',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+    },
+]
+console.log(teamMember);
