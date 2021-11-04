a651e48f8ff8f38373bcce30ef31396bbdb6bda8
DanyMencu <dany69men@gmail.com>
2021-11-04 14:34:30 +0100

    Project draft created

---
+++ script.js
@@ -0,0 +1,5 @@
+//*1- Creare un array di oggetti che rappresenta ciascun membro del team. 
+//Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+//*2- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+//*3- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+
